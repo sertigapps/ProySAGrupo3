8e2e4b21054232ddf4963809537949b9e807366e
tejediego <diegotejeda93@gmail.com>
2019-05-10 22:32:29 -0600

    Script casi final

--- Tiendas - BD/Script.sql
+++ Tiendas - BD/Script.sql
@@ -13,6 +13,7 @@ CREATE TABLE Tienda(
 CREATE TABLE Bodega(
 	cod_bodega INT AUTO_INCREMENT,
 	nombre VARCHAR(50) NOT NULL,
+	pais VARCHAR(25) NOT NULL,
 	cod_tienda INT NOT NULL,
 	PRIMARY KEY(cod_bodega),
 	FOREIGN KEY (cod_tienda)
@@ -61,13 +62,62 @@ CREATE TABLE Imagen(
 CREATE TABLE Cliente(
 	cod_cliente INT AUTO_INCREMENT,
 	nombre VARCHAR(25) NULL,
-	direccion TEXT NOT NULL
+	pais VARCHAR(25) NOT NULL,
+	direccion TEXT NOT NULL,
+	PRIMARY KEY (cod_cliente)
+);
+
+CREATE TABLE Estado(
+	cod_estado INT AUTO_INCREMENT,
+	nombre VARCHAR(25) NOT NULL,
+	PRIMARY KEY (cod_estado)
 );
 
 CREATE TABLE Orden(
-
+	cod_orden INT AUTO_INCREMENT, 
+	total FLOAT,
+	cod_cliente INT NOT NULL,
+	cod_estado INT NOT NULL,
+	PRIMARY KEY (cod_orden), 
+	FOREIGN KEY (cod_cliente)
+		REFERENCES Cliente(cod_cliente),
+	FOREIGN KEY (cod_estado)
+		REFERENCES Estado(cod_estado)
 );
 
-CREATE TABLE Detalle_Orden(
-
+CREATE TABLE Detalle_Orden( 
+	cod_det_orden INT AUTO_INCREMENT,
+	cod_prod INT NOT NULL, 
+	cod_tienda INT NOT NULL, 
+	cod_orden INT NOT NULL,
+	PRIMARY KEY (cod_det_orden),
+	FOREIGN KEY (cod_prod)
+		REFERENCES Producto (SKU),
+	FOREIGN KEY (cod_tienda)
+		REFERENCES Tienda(cod_tienda),
+	FOREIGN KEY (cod_orden)
+		REFERENCES Orden(cod_orden)	
 );
+
+CREATE TABLE Detalle_Bodega( 
+	cod_det_orden  INT NOT NULL,
+	cod_bodega INT NOT NULL,
+	PRIMARY KEY (cod_det_orden, cod_bodega), 
+	FOREIGN KEY(cod_bodega)
+		REFERENCES Bodega(cod_bodega), 
+	FOREIGN KEY (cod_det_orden)
+		REFERENCES Detalle_Orden(cod_det_orden)
+);
+
+--Borrar
+--DROP TABLE Detalle_Bodega;
+--DROP TABLE Detalle_Orden;
+--DROP TABLE Orden;
+--DROP TABLE Estado; 
+--DROP TABLE Cliente; 
+--DROP TABLE Imagen;
+--DROP TABLE Categoria_Producto;
+--DROP TABLE Categoria;
+--DROP TABLE Producto;
+--DROP TABLE Bodega;
+--DROP TABLE Tienda;
